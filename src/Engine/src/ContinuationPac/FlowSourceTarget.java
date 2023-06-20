2a858c5b67c6cef0fce7db60fcc5305790b3d4d9
Sharon Pais <mrshapron@gmail.com>
2023-06-21 00:06:47 +0300

    removing unnecessary classes

--- src/Engine/src/ContinuationPac/FlowSourceTarget.java
+++
@@ -1,8 +0,0 @@
-package ContinuationPac;
-
-import Flow.Definition.FlowDefinition;
-
-public interface FlowSourceTarget {
-    String source();
-    String target();
-}
--- src/Engine/src/Convert/FlowConverter.java
+++ src/Engine/src/Convert/FlowConverter.java
@@ -2,13 +2,11 @@ package Convert;
 
 
 import ContinuationPac.ContinuationMetaData;
-import ContinuationPac.FlowSourceTarget;
 import Flow.Definition.FlowDefinition;
 import JAXB.Generated.STFlow;
 import JAXB.Generated.STStepper;
 
 import java.util.List;
-import java.util.Map;
 
 public interface FlowConverter {
     FlowDefinition Convert(STFlow flowJAXB);
