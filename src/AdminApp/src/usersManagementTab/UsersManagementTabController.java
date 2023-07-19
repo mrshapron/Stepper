a07ec1ae3932d69a427be1f0ded3ab373d1bab67
Gur Shmuelevitz <gur_shmu@hotmail.com>
2023-07-20 00:22:29 +0300

    Fetching data about users from the server into UsersManagementTabController And updating the tableView

--- src/AdminApp/src/usersManagementTab/UsersManagementTabController.java
+++ src/AdminApp/src/usersManagementTab/UsersManagementTabController.java
@@ -1,4 +1,73 @@
 package usersManagementTab;
 
+import Users.UserImpl;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import usersManagementTab.ViewModel.UserModelView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsersManagementTabController {
+//    List<UserImpl> currUsers;
+
+    @FXML
+    private TableView<UserModelView> UsersTableView;
+
+    @FXML
+    private TableColumn<UserModelView, String> UserNameColumn;
+
+    @FXML
+    private TableColumn<UserModelView, String> NumberOfRolesColumn;
+
+    @FXML
+    private TableColumn<UserModelView, String> ManagerColumn;
+
+    @FXML
+    private Label UserNameLabel;
+
+    @FXML
+    private ListView<?> UserDetailsListView;
+
+    @FXML
+    private TableView<?> AssignedFlowsTableView;
+
+    @FXML
+    private TableColumn<?, ?> AssignedRoleNameColumn;
+
+    @FXML
+    private TableColumn<?, ?> AssignedRolesDescriptionColumn;
+
+    public void initialize() {
+        // Set up cell value factories for the table columns
+        UserNameColumn.setCellValueFactory(cellData -> cellData.getValue().usernameProperty());
+        NumberOfRolesColumn.setCellValueFactory(cellData -> cellData.getValue().numberOfRolesProperty());
+        ManagerColumn.setCellValueFactory(cellData -> cellData.getValue().managerProperty());
+    }
+
+    public void updateUsers(List<UserImpl> updatedUsers) {
+        // Convert the UserImpl objects to UserModel and update the table
+        List<UserModelView> usersList = convertToUserModelList(updatedUsers);
+        UsersTableView.getItems().setAll(usersList);
+    }
+
+    private List<UserModelView> convertToUserModelList(List<UserImpl> users) {
+        // Convert UserImpl objects to UserModel objects
+        List<UserModelView> userModelList = new ArrayList<>();
+        for (UserImpl user : users) {
+            String username = user.username();
+            String numberOfRoles = String.valueOf(user.role()); // Assuming you have a getRoles() method
+            String manager = "true"; // Replace this with the actual getter for manager
+
+            UserModelView userModel = new UserModelView(username, numberOfRoles, manager);
+            userModelList.add(userModel);
+        }
+        return userModelList;
+    }
 }
+
